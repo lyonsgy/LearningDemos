5a085719711c2b46a57ff05518a0469b8abab138
gy_lyons <gy_lyons@163.com>
2017-11-14 23:21:32 +0800

    MVVMDemo

---
+++ MVVMDemo/MVVMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMDemo
+//
+//  Created by lyons on 2017/11/14.
+//  Copyright © 2017年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMDemo/MVVMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMDemo
+//
+//  Created by lyons on 2017/11/14.
+//  Copyright © 2017年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
