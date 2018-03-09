75129a01f9a3316e4062dcf8b6aa4fd102f57615
gy_lyons <gy_lyons@163.com>
2018-03-09 17:19:42 +0800

    MyProject

---
+++ MYProject/MYProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYProject
+//
+//  Created by lyons on 2018/3/8.
+//  Copyright © 2018年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYProject/MYProject/LPZRequest/LPZRequest.h
@@ -0,0 +1,13 @@
+//
+//  LPZRequest.h
+//  MYProject
+//
+//  Created by lyons on 2018/3/8.
+//  Copyright © 2018年 GY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPZRequest : NSObject
+
+@end
